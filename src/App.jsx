9348f0d9e21bdd194f40b431fdc5cba5bcd5efa9
mardhanawn <mardhanawn@gmail.com>
2023-08-26 17:21:52 +0700

    update container className to all content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from './components/Footer'
+import DiscoverMovies from './pages/DiscoverMovies'
 import './App.css'
 
 function App() {
     return (
         <>
+            <DiscoverMovies />
             <Footer />
         </>
     )
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,24 +2,26 @@ import MoovieTimeBlack from '../../assets/MoovieTimeBlack.png'
 
 function Footer() {
     return (
-        <div className="d-flex row my-5">
-            <div
-                className="col-lg-4 mt-lg-0 mt-md-5 d-flex d-md-flex justify-content-center"
-                style={{ color: '#929292' }}
-            >
-                2021 MoovieTime. All right reserved
-            </div>
-            <div
-                className="col-lg-4 mt-5 mt-lg-0 mt-md-5 d-flex d-md-flex justify-content-center"
-                style={{ color: '#929292' }}
-            >
-                <img src={MoovieTimeBlack} style={{ color: '#929292' }} />
-            </div>
-            <div
-                className="col-lg-4 mt-5 mt-lg-0 mt-md-5 d-flex d-md-flex justify-content-center"
-                style={{ color: '#929292' }}
-            >
-                Made with React by ardhanaawahyu
+        <div className="container-lg my-5">
+            <div className="row">
+                <div
+                    className="col-lg-4 mt-lg-0 mt-md-5 d-flex d-md-flex justify-content-center"
+                    style={{ color: '#929292' }}
+                >
+                    2021 MoovieTime. All right reserved
+                </div>
+                <div
+                    className="col-lg-4 mt-5 mt-lg-0 mt-md-5 d-flex d-md-flex justify-content-center"
+                    style={{ color: '#929292' }}
+                >
+                    <img src={MoovieTimeBlack} style={{ color: '#929292' }} />
+                </div>
+                <div
+                    className="col-lg-4 mt-5 mt-lg-0 mt-md-5 d-flex d-md-flex justify-content-center"
+                    style={{ color: '#929292' }}
+                >
+                    Made with React by ardhanaawahyu
+                </div>
             </div>
         </div>
     )
